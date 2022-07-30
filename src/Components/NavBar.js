6659a3226794308d01e59462d7550413c9f8ae93
Mason Mei <masonmei@pursuit.org>
2022-07-30 18:51:33 -0500

    removed fontawesome

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGem } from "@fortawesome/free-solid-svg-icons";
 export default function NavBar() {
   return (
     <nav>
        <h1>
        
-        <Link to="/"><FontAwesomeIcon icon={faGem}></FontAwesomeIcon> Safu</Link>
+        <Link to="/"> Safu</Link>
       </h1>
       <ul>
         <Link to="/transactions">Transactions</Link>
